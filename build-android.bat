b2bde9661a384058cc05d650165c65aa927bf872
Rostislav Lastochkin <remtrik@mail.ru>
2026-07-12 03:50:08 +0300

    change build-android.bat to use fastforge

--- build-android.bat
+++ build-android.bat
@@ -2,4 +2,4 @@ flutter clean
 flutter pub get
 dart pub global activate fastforge
 dart setup.dart android --out core
-flutter build apk --release --obfuscate --split-debug-info=debug-symbols/ --split-per-abi
+dart pub global run fastforge:main release --name stable-android
