25afb272598a22848e15c1a24c19812e7bb69b98
Rostislav Lastochkin <remtrik@mail.ru>
2026-08-26 13:44:26 +0300

    Remove leftover icon in profiles

--- lib/views/profiles/profiles.dart
+++ lib/views/profiles/profiles.dart
@@ -344,14 +344,6 @@ class _ProfileItemState extends State<ProfileItem> {
           );
         }),
       const SizedBox(height: 6),
-      const SizedBox(
-              width: 24,
-              height: 24,
-              child: CircleAvatar(
-                foregroundImage: AssetImage("assets/images/icon.png"),
-                backgroundColor: Colors.transparent,
-              ),
-            ),
       Text(
         expireDate != "N/A"
             ? '${appLocalizations.expiresOn} $expireDate'
